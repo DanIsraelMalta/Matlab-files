4925f0c5d8a63801a817b0e53dd5244c3044a290
Dan Malta <malta.dan@gmail.com>
2015-10-26 15:29:30 +0200

    Update scatterReduction.m

--- curve-fitting-regression/scatterReduction.m
+++ curve-fitting-regression/scatterReduction.m
@@ -1,8 +1,7 @@
 %{
-% scatterReduction is my attempt to create sort of a scatterplot interpolant based on
-% grouped first order difference smoothing.
-% basically, its an attempt to reduce scattered data size while maintaing
-% most of the data.
+% scatterReduction is my attempt to create sort of a 'scatterplot interpolant based on
+% grouped first order difference smoothing'. basically, its an attempt to reduce scattered
+% data size while maintaing most of the information.
 %
 % x, y - scattered data to be smoothed & reduced
 % n - number of bins (of which x shall be divided to)
@@ -16,8 +15,19 @@ x = 0 : 0.01: 12*pi;
 y = sin(x) + 0.2 * randn(size(x));
 [u, z] = scatterReduction(x, y, 40, 1);
 figure;
+subplot(2,1,1);
 plot(x, y, '-or', u, z, '-*b');
 legend('original data set', 'smoothed/reduced data set');
+title(['Example #1 - smoothed & reduced original ser from ', num2str(numel(x)), ' points to ', num2str(numel(u)), ' points. Thats ', num2str(100 * numel(x) / numel(u)), '% data reduction.']);
+grid on;
+
+x=linspace(1,10,100);
+y=exp(-0.666 * x) + 4.0 + 0.05*randn(1,100);
+[u, z] = scatterReduction(x, y, 30, 1);
+subplot(2,1,2);
+plot(x, y, '-or', u, z, '-*b');
+legend('original data set', 'smoothed/reduced data set');
+title(['Example #1 - smoothed & reduced original ser from ', num2str(numel(x)), ' points to ', num2str(numel(u)), ' points. Thats ', num2str(100 * numel(x) / numel(u)), '% data reduction.']);
 grid on;
 
 %
@@ -33,14 +43,14 @@ function [u, z] = scatterReduction(x, y, n, beta)
     s = zeros(1, n); 
     xmin = min(x);
     dx = (max(x) - xmin) / n;
-    
+
     % grouping number and sum of observations
     for i = 1 : length(x)
         j = min(1 + floor((x(i) - xmin) / dx), n);
         c(j) = c(j) + 1;
         s(j) = s(j) + y(i);
     end
-        
+
     % first order difference smoothing
     p = diff(speye(n), 1);
     z = (diag(c) + beta * p' * p) \ s';
